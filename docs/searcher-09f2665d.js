d3fb1732eec915b7603b58cdfff21b5ba7952325
iiiii7d <iiiii7d@users.noreply.github.com>
2026-07-03 06:01:08 +0000

    deploy: 543460ed537859fb272a1b20089590d638c3b90d

--- docs/searcher-09f2665d.js
+++ docs/searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-54b81178.js',
+                path_to_root + 'searchindex-a6b9020b.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
